52eb5e664daf7631100fba42623b08aca980d42b
Usee <yasno.ponyatno229@gmail.com>
2024-01-16 21:00:32 +0300

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        int startBalance = 100;
+        int added = 1500;
+
+        int bonus = added > 1000 ? added / 100 : 0;
+
+        int finalBalance = startBalance + added + bonus;
+        System.out.println("Итоговый счет:" +finalBalance);
+        System.out.println("Бонус:" + bonus);
         
+
+
     }
 }
